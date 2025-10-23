8a798e8fe8371cd5f5aa91dc98627a6f0afb523e
syrex1013 <remix3030303@hotmail.com>
2025-10-23 21:11:08 +0200

    Switch to npm-based installation (binaries not working with ESM)

--- derp.rb
+++ derp.rb
@@ -1,22 +1,18 @@
 class Derp < Formula
   desc "Natural language grep for regex-challenged developers"
   homepage "https://github.com/syrex1013/derp"
-  version "0.1.1"
+  url "https://registry.npmjs.org/derpgrep/-/derpgrep-0.1.1.tgz"
+  sha256 "PLACEHOLDER"
+  license "MIT"
 
-  if OS.mac?
-    url "https://github.com/syrex1013/derp/releases/download/v#{version}/derp-macos"
-    sha256 "8bf7d70c61f4f3accc97ea721bb719fe875ef03165fe810c4bd88d5bf8735e50" # macos
-  elsif OS.linux?
-    url "https://github.com/syrex1013/derp/releases/download/v#{version}/derp-linux"
-    sha256 "78e05908149f6cfd53276b441b47aef8e21a9a78a29ef91e70a6744f1b94c91e" # linux
-  end
+  depends_on "node"
 
   def install
-    bin.install "derp-macos" => "derp" if OS.mac?
-    bin.install "derp-linux" => "derp" if OS.linux?
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    system "#{bin}/derp", "--version"
+    system bin/"derp", "--version"
   end
 end
