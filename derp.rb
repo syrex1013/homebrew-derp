4d9d4d84585fd76f20c885898ab99476f4249eef
syrex1013 <remix3030303@hotmail.com>
2025-10-23 21:13:38 +0200

    Add correct SHA256 for npm package

--- derp.rb
+++ derp.rb
@@ -2,7 +2,7 @@ class Derp < Formula
   desc "Natural language grep for regex-challenged developers"
   homepage "https://github.com/syrex1013/derp"
   url "https://registry.npmjs.org/derpgrep/-/derpgrep-0.1.1.tgz"
-  sha256 "PLACEHOLDER"
+  sha256 "f898b7196abdfdbf3fb74a5e16a0155f81da7a742d6f9ffa9d82166bbc021375"
   license "MIT"
 
   depends_on "node"
